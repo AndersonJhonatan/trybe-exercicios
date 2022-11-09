c0bf90af4412e7a2448f62adcb5eaf001e478ee6
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2022-11-09 01:43:23 -0300

    Create script.js

---
+++ fundamentos/dia-JavaScript-ES6 - let-const/dia7-2-fluxoDeObejtos/Exercicio 1/script.js
@@ -0,0 +1,20 @@
+/* const value1 = +document.getElementById("value1").value;
+const value2 = +document.getElementById("value2").value;
+
+const sum = (value1, value2) => {
+  if (typeof value1 !== "number" || typeof value2 !== "number") {
+    throw new Error(alert("Os valores devem ser numéricos"));
+  }
+};
+const result = (value1, value2) => {
+  try {
+    sum(value1, value2);
+    return value1 + value2;
+  } catch (error) {
+    return error.message;
+  }
+};
+window.onload = () => {
+  const button = document.getElementById("button");
+  button.addEventListener("click", sum);
+}; */
