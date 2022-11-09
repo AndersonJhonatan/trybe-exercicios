5a1680e8403c8b29e8ae5998c66c1d0dff718010
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2022-11-09 01:44:33 -0300

    Create script.js

---
+++ fundamentos/javascript-Dom-eventos/dia-5.1/script.js
@@ -0,0 +1,11 @@
+document.getElementsByTagName("p")[1].innerText = "Estarei estudando muito melhor do que hoje e melhor do que amanha";
+
+document.getElementsByTagName('main')[0].style.backgroundColor = "rgb(76,164,109)";
+
+document.getElementsByTagName("section")[0].style.backgroundColor = "white"
+
+document.getElementsByTagName("h1")[0].innerText = "Exercício - JavaScript"
+
+document.getElementsByTagName('p')[0].innerText ="Texto padrão do nosso site".toUpperCase()
+
+document.getElementsByTagName('p')
