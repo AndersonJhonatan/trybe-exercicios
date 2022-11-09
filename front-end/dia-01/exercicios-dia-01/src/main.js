8e9d1ff14ac7793465e34f1f41872e343cb274c0
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2022-11-09 11:31:04 -0300

    Update main.js

--- front-end/dia-01/exercicios-dia-01/src/main.js
+++ front-end/dia-01/exercicios-dia-01/src/main.js
@@ -1,1 +1,20 @@
-import { nanoid } from 'nanoid'
+import validator from "validator";
+
+const inputTexto = document.querySelector('#value');
+const button = document.querySelector('#button');
+const  option = document.querySelector('#option');
+const answer = document.querySelector('#answer');
+
+button.addEventListener('click', (event) => {
+event.preventDefault();
+})
+
+const campos = {
+  cpf: validator.isTaxID(inputTexto.value, 'pt-BR'),
+  hexColor: validator.isHexColor(inputTexto.value),
+  email: validator.isEmail(inputTexto.value),
+  uuid: validator.isUUID(inputTexto.value, 4),
+  url: validator.isURL(inputTexto.value),
+};
+
+answer.innerHTML = `A validação retornou ${campos[option.value]}`;
