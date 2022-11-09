bac18bad74529c595f9f43f7f74242028475db15
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2022-11-09 01:44:38 -0300

    Create seletores.js

---
+++ fundamentos/javascript-Dom-eventos/dia-5.1/seletores.js
@@ -0,0 +1,6 @@
+document.getElementById("container").style.backgroundColor = "black";
+document.getElementById("header-container").style.backgroundColor = "green";
+document.getElementsByTagName("section")[0].style.backgroundColor = "yellow";
+document.getElementsByTagName("section")[1].style.backgroundColor = "blue";
+document.getElementById("footer-container").style.backgroundColor = "green";
+document.querySelectorAll("emergency-tasks");
